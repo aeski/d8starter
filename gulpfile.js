c299a720469236ef8a2c95addd87bbe51664ae4d
Kit Whyte <kit@aeski.net>
2020-05-15 23:15:55 +1000

    typography split out partial scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,94 @@
-var gulp = require('gulp'),
-   sass = require('gulp-sass'),
-   minifycss = require('gulp-minify-css'),
-//    uglify = require('gulp-uglify'),
-   rename = require('gulp-rename'),
-   livereload = require('gulp-livereload'),
-//    clean = require('gulp-clean'),
-//    pixrem = require('gulp-pixrem'),
-   autoprefixer = require('gulp-autoprefixer');
-//    concat = require('gulp-concat');
+var theme = "D8 Starter";
+var gulp  = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cleanCss = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    lazypipe = require('lazypipe'),
+    postcss = require('gulp-postcss'),
+    //connect = require('gulp-connect'),
+    autoprefixer = require('autoprefixer');
+  
 
-var SRC_DIR = '',
-   DEST_DIR = '';
+var compileTheme = lazypipe()
+        .pipe(sourcemaps.init)
+        .pipe(function () {
+            return sass().on('error', sass.logError);
+        })
+        .pipe(postcss, [ autoprefixer()])
+         .pipe(sourcemaps.write)
+         .pipe(rename, {basename: 'style'});
+		 var minifyCss = lazypipe()
+         .pipe(cleanCss)
+         .pipe(rename, {suffix: '.min'});
+         
 
-gulp.task('styles', function() {
- return gulp.src([
-   SRC_DIR + 'scss/**/style.scss',
-   ])
-   .pipe(sass())
-   .pipe(autoprefixer({
-	   browsers: ['last 3 versions'],
-	   safe: true
-   }))
-   .pipe(gulp.dest( DEST_DIR + 'css/' ))
-   .pipe(rename({suffix: '.min'}))
-   .pipe(minifycss())
-   .pipe(gulp.dest( DEST_DIR + 'css'))
-   .pipe(livereload());
+
+
+
+gulp.task('watch', () => {
+	gulp.watch('./*.html', (done) => {
+        gulp.series(['html'])(done);
+    });
+
+    gulp.watch('scss/**/*.scss', (done) => {
+        gulp.series(['styles'])(done);
+    });
+
 });
 
 
-// gulp.task('clean', function() {
-//  return gulp.src([DEST_DIR + 'css'], {read: false})
-//    .pipe(clean());
-// });
+
+//gulp.task('watch', ['default', 'connect'], function () {
+//    gulp.watch(['scss/**/*.scss'], ['styles']);
+//    gulp.watch(['./*.html'], ['html']);
+//});
+/*
+
+gulp.task('connect', function () {
+    connect.server({
+        livereload: true,
+        port: 5050
+    });
+});
+*/
+
+/*
+gulp.task('connect', () => {
+  connect.server({
+        livereload: true,
+        port: 5050
+    });
 
 
-gulp.task('watch', function() {
- gulp.watch(SRC_DIR + 'scss/*.scss', ['styles']);
- 
- livereload.listen();
- 
- 
+});
+*/
+/*
+gulp.task('html', function () {
+    gulp.src('./*.html')
+        .pipe(connect.reload());
+});
+*/
+
+gulp.task('html', () => {
+	
+    return gulp.src('./*.html')
+    .pipe(connect.reload());
 });
 
-
-gulp.task('default', function() {
- gulp.start('styles', 'watch');
+// Main task compiling CSS files from SCSS
+gulp.task('styles', () => {
+    return gulp.src('scss/*.scss')
+		.pipe(compileTheme())
+//		.pipe(sourcemaps.init)
+ //       .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./css'))
+        //.pipe(connect.reload());
 });
 
-
+gulp.task('default', gulp.series(['watch']));
+//gulp.task('default', gulp.parallel(['connect', 'watch']));
+//gulp.task('default', gulp.series(['connect', 'watch']));
+//gulp.task('default', gulp.parallel(['styles', 'connect', 'html', 'watch']));
--- js/script.js
+++ js/script.js
@@ -4,15 +4,52 @@ window.addEventListener("load", () => {
 
 // Not adding jQuery by default
 //jQuery(document).ready(function($) {
-/*
-	var $hamburger = $("#hamburger");
-	$hamburger.on("click", function(e) {
-	$hamburger.toggleClass("is-active");
-		// Do something else, like open/close menu
-	});
-*/
-  
 
+
+/*
+            document.addEventListener(
+                "DOMContentLoaded", () => {
+                    new Mmenu( "#mmenu-custom", {
+			"slidingSubmenus": true,
+			"extensions": [
+				"pagedim-black",
+				"position-right"
+			],
+			"navbars": [{
+				"position": "top",
+	            "content": [
+					"<div class='mmenu-header' href='/'></div>"
+					]
+				},
+				{
+				"position": "bottom",
+				"content": [
+					"<div class='mmenu-footer'></div>"
+					]
+				},
+				{
+				"position": "top",
+				"content": [
+					"<a class='link twitter' href='https://twitter.com/womenshealthtas?lang=en'><i class='fa fa-twitter'></i>twit</a>",
+					"<a href='tel:62313212'><i class='fa fa-phone'></i>call</a>",
+					"<a class='link facebook' href='https://www.facebook.com/Womens-Health-Tasmania-106238739430050/'><i class='fa fa-facebook'></i>facebook</a>"
+				]
+			}]
+		});
+
+
+   
+                }
+                
+            );
+        const primary = document.getElementById("thing");
+document.getElementById('hamburger').addEventListener('click', function() {
+  console.log('anchor');
+  primary.click();      
+
+}); 
+*/
+          
 	  
 	// Remove class is-active class when closing 
 	//$('.mm-wrapper__blocker').on('click mousedown touchstart', function() {
